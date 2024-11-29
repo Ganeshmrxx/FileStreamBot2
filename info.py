8db00346fe0fc536e3ccb10c102cadccd5a5f11b
Ganesh <100681292+Ganeshmrxx@users.noreply.github.com>
2024-11-29 15:47:12 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,30 @@
+
+
+officialchatid = -1001975298098
+
+bot_username = "MuviBoxBot"
+
+updatechannel = "muvi_official"
+MUVI_Bots="https://t.me/muvibots"
+Muvi_requested_Files = -1002145411468
+
+logDataChannel = -1001942141488  #newLOgdata2in1
+DB01_MB  = -1001859775796 #db01_mb
+DB02_MB = -1002059529731 #db02_mb
+
+botid = 6395528696 #MuviBoxBot
+
+#welcom gifs
+
+
+gif1 = "https://graph.org/file/c503a544b71b7672fa269.gif"
+
+gif2 = "https://graph.org/file/31c84eb47d457a03eabf2.gif"
+
+gif3 = "https://graph.org/file/e34949b2f29e279e8dce7.gif"
+
+gif4="https://graph.org/file/10bbdeb7814407e3c88f6.gif"
+
+gif5="https://graph.org/file/1297008d976c9d2f048c9.gif"
+
+gif6 = "https://graph.org/file/f898bc9308f53eca2dfa8.gif"
